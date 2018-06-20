bde6db208eea266d63abe74cccca2c2fb7d6a4ab
Jon Foust <jonfoust@google.com>
2018-06-20 14:25:53 -0400

    Initial
    Initial commit of FPS from book. First commit starts from chapter 3.

---
+++ Assets/FPSInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour {
+    public float speed = 6.0f;
+    private CharacterController _charController;
+    public float gravity = -9.8f;
+	// Use this for initialization
+	void Start () {
+        _charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Translate(0, speed, 0);
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+
+        //transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+	}
+}
